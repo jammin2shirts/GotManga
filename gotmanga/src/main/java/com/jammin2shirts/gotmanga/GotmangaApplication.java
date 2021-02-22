428a89b2212c6c7f501c01af9eed6aac33d32fb5
jammin2shirts <teuchertb@gmail.com>
2021-02-21 23:18:35 -0500

    created java app

---
+++ gotmanga/src/main/java/com/jammin2shirts/gotmanga/GotmangaApplication.java
@@ -0,0 +1,13 @@
+package com.jammin2shirts.gotmanga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotmangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotmangaApplication.class, args);
+	}
+
+}
---
+++ gotmanga/src/test/java/com/jammin2shirts/gotmanga/GotmangaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jammin2shirts.gotmanga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotmangaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
